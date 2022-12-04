1340c9513310492bd8f20971860a5fb500d8f90f
rylan-vrar <tinkerguy1@gmail.com>
2022-12-04 12:50:34 -0800

    RPS Logic + AI

---
+++ Assets/Scripts/OptionTarget.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionTarget : MonoBehaviour
+{
+    public string optionName;
+
+    private Renderer renderer;
+    private bool hovered = false;
+    private RPSManager rpsManager;
+
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        rpsManager = GameObject.Find("RPS Manager").GetComponent<RPSManager>();
+    }
+
+
+    private void OnMouseEnter()
+    {
+        hovered = true;
+        renderer.material.color = Color.green;
+    }
+
+    private void OnMouseDown()
+    {
+        renderer.material.color = Color.yellow;
+        rpsManager.player1DecisionText.text = "P1 Decision: " + optionName;
+        rpsManager.GenerateAIDecision();
+    }
+
+    private void OnMouseUp()
+    {
+        if (hovered) { renderer.material.color = Color.green; }
+        else if (!hovered) { renderer.material.color = Color.white; }
+    }
+
+    private void OnMouseExit()
+    {
+        hovered = false;
+        renderer.material.color = Color.white;
+    }
+}
